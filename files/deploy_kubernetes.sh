586d80e599c01ad315853991088b8667e8717279
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-19 13:50:45 +0000

    Small fix in CI/CD #3

--- files/deploy_kubernetes.sh
+++ files/deploy_kubernetes.sh
@@ -2,6 +2,8 @@
 set -e # ensure that this script will return a non-0 status code if any of rhe commands fail
 set -o pipefail # ensure that this script will return a non-0 status code if any of rhe commands fail
 
+VERSION=$1
+
 cat << EOF > service.yaml
 
 ---
